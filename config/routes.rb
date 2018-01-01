d661d2266e57bb392c9cbdff227bacf02a0c641e
Mitsuhiro ETO <frn.fnts@gmail.com>
2018-01-01 17:08:27 +0900

    add a route for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "application#index"
+  devise_for :users, path_names: {current_user: 'me'}
+  # devise_for :users, :controllers => {
+  #    :registrations => 'users/registrations'
+  # }
 end
