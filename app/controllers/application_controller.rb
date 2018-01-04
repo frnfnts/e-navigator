6a5e4b2d0ba259cd21bef838e18ae2054e0d141a
Mitsuhiro ETO <frn.fnts@gmail.com>
2018-01-04 17:13:49 +0900

    change route for /

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def index
-    render html: 'hello world!'
+    if user_signed_in?
+      redirect_to edit_me_path
+    else
+      redirect_to new_me_session_path
+    end
   end
 end
